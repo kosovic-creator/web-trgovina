27c395da1611e70934fd79b73ce839b83296fe29
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-19 06:25:36 +0200

    prisma2

--- lib/auth.ts
+++
@@ -1,7 +0,0 @@
-export async function getUser(email: string, password: string) {
-  // Ovde ide logika za proveru korisnika u bazi
-  // Primer:
-  // const user = await db.user.findUnique({ where: { email } });
-  // if (user && user.password === password) return user;
-  // return null;
-}
