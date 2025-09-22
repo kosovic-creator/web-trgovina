7f4839ebe522c4b7712c1b38e0656570587209ad
kosovic-creator <drasko.kosovic@icloud.com>
2025-09-23 00:38:02 +0200

    gg

--- app/api/proizvodi/[id]/route.ts
+++ app/api/proizvodi/[id]/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(req: Request) {
+    const url = new URL(req.url);
+    const id = url.pathname.split('/').pop();
   if (!id) {
     return NextResponse.json({ error: 'ID je obavezan.' }, { status: 400 });
   }
